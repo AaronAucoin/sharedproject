b37ae8ec83af7d6fcf9a0945d123fc0ef46ab4df
Matthew Balachowski <Mbalac3@lsu.edu>
2025-02-10 16:47:50 -0600

    mattwuzhere

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'AaronWuzHere JuliaWuzHere'),
+      home: const MyHomePage(title: 'AaronWuzHere JuliaWuzHere MatthewBalachowskiWuZzHere'),
     );
   }
 }
